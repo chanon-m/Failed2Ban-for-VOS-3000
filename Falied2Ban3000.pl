7cdb397dffe40be1a5f536c060c0b3eae4a7754d
Chanon <chanon_m@hotmail.com>
2015-03-13 03:30:32 -0700

    Update Falied2Ban3000.pl

--- Falied2Ban3000.pl
+++ Falied2Ban3000.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 use strict;
 use DBI;
 use File::Copy;
@@ -10,6 +10,7 @@ my $db = DBI->connect(
      { RaiseError => 1 },
 ) or die $DBI::errstr;
 
+#Query illegal calls
 my $query = $db->prepare("select DISTINCT callerip from e_cdr where endreason = '-9'");
 $query->execute();
 
@@ -24,17 +25,17 @@ while ($rows = $query->fetchrow_arrayref()) {
 $query->finish();
 $db->disconnect();
 
-#read iptables configuration
+#Read iptables configuration
 open(my $fh, '<',"/etc/sysconfig/iptables") or die "Could not open file!\n";
 my @lines=<$fh>;
 my $linenum = scalar(@lines) - 2;
 close $fh;
 
-#backup iptables file
+#Backup iptables file
 my $t = localtime;
 move("/etc/sysconfig/iptables","/etc/sysconfig/iptables.$t") or die "Could not backup file!\n";
 
-#Updated an iptables configuration file
+#Update an iptables configuration file
 my $i=0;
 open(my $fh, '>',"/etc/sysconfig/iptables") or die "Could not open file!\n";
 foreach my $line (@lines) {
@@ -50,7 +51,7 @@ foreach my $line (@lines) {
       for(my $j=0; $j < $count; $j++) {
         if($heckip[$j]!="") {
           print $fh "-A RH-Firewall-1-INPUT -s $heckip[$j] -j DROP\n";
-          #Updated a rule in IPtables
+          #Update a rule in IPtables
           my $returncode = system("/sbin/iptables -A RH-Firewall-1-INPUT -s $heckip[$j] -j DROP");
           if($returncode != 0) {
               print "Falied to add rules in iptables!\n";
